34a5e341ed992175dbc025321c306745b78181f9
alex <alex@noemail.net>
2018-08-17 11:42:21 +0000

    added examples 4+5
    FossilOrigin-Name: f4f5f9cdf6643834b4a7e9a712f20c402162b413e584c0fee8cc2fe32ce9c7e5

---
+++ examples/sample5.tcl
@@ -0,0 +1,16 @@
+#!/bin/sh
+#\
+exec tclsh8.6 "$0" "$@"
+
+#package require ooxml
+source ../ooxml.tcl
+
+set spreadsheet [::ooxml::xl_write new -creator {Alexander Schöpe}]
+if {[set sheet [$spreadsheet worksheet {Tabelle 1}]] > -1} {
+  set date [$spreadsheet style -numfmt [$spreadsheet numberformat -datetime]]
+  $spreadsheet defaultdatestyle $date
+  # 2018-03-02 17:39 -> 43161.73542
+  $spreadsheet cell $sheet "2018-03-02 17:39" -index 0,0
+  $spreadsheet write export5.xlsx
+}
+$spreadsheet destroy

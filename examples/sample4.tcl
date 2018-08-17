f8cf0f18a5cd48ce1f8e5422cc2c0b2886c2bfe5
alex <fossil@sowaswie.de>
2018-08-17 11:42:21 +0000

    added examples 4+5
    FossilOrigin-Name: f4f5f9cdf6643834b4a7e9a712f20c402162b413e584c0fee8cc2fe32ce9c7e5

---
+++ examples/sample4.tcl
@@ -0,0 +1,19 @@
+#!/bin/sh
+#\
+exec tclsh8.6 "$0" "$@"
+
+lappend auto_path .
+package require ooxml
+
+source array.tcl
+
+set spreadsheet [::ooxml::xl_write new -creator {Alexander Schöpe}]
+if {[set sheet [$spreadsheet worksheet {Tabelle 1}]] > -1} {
+  $spreadsheet row $sheet
+  $spreadsheet cell $sheet 3
+  $spreadsheet cell $sheet 5
+  $spreadsheet cell $sheet {} -formula A1+B1
+
+  $spreadsheet write export4.xlsx
+}
+$spreadsheet destroy

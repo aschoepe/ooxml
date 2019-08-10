47f9608b9341cbbef2391382b761be729f76d9d0
alex <fossil@sowaswie.de>
2019-08-10 09:15:13 +0000

    pkg req modification
    FossilOrigin-Name: a3e6c25ebfa518e2b3fec389a26841248e54341039fdeae59e0acd5b0c5e140e

--- examples/sample1.tcl
+++ examples/sample1.tcl
@@ -2,8 +2,10 @@
 #\
 exec tclsh8.6 "$0" "$@"
 
-#package require ooxml
-source ../ooxml.tcl
+set auto_path [linsert $auto_path 0 ..]
+if {[catch {package require ooxml}]} {
+  source ../ooxml.tcl
+}
 
 source array.tcl
 
--- examples/sample5.tcl
+++ examples/sample5.tcl
@@ -2,8 +2,10 @@
 #\
 exec tclsh8.6 "$0" "$@"
 
-#package require ooxml
-source ../ooxml.tcl
+set auto_path [linsert $auto_path 0 ..]
+if {[catch {package require ooxml}]} {
+  source ../ooxml.tcl
+}
 
 set spreadsheet [::ooxml::xl_write new -creator {Alexander Schöpe}]
 if {[set sheet [$spreadsheet worksheet {Tabelle 1}]] > -1} {

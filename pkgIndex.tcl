d80225e841675916c2d1da171692158ee61b609f
alex <fossil@sowaswie.de>
2019-10-30 22:05:29 +0000

    TEA support (Tcl Extension Architecture)
    FossilOrigin-Name: 03f6df59a495883462514c066c3abf0bda37fe6527059a27d2bd85b8dcdadcc5

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,2 +1,4 @@
-if {![package vsatisfies [package provide Tcl] 8.6]} return
-package ifneeded ooxml 1.3 [list source [file join $dir ooxml.tcl]]
+#
+# Tcl package index file
+#
+package ifneeded ooxml 1.3 [list source [file join $dir ooxml.tcl]

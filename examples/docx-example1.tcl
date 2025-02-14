814b5e7f5534cbb6d084feb72684fa33854e4a34
rolf <rolf@pointsman.de>
2025-02-14 00:25:25 +0000

    Added a first somewhat helpful example file to examples. docxtest.tcl will go away at some point.
    FossilOrigin-Name: 848ee97430da67eb3cc3a78461e0332ba862e6767321762ad1553675e8926dc7

---
+++ examples/docx-example1.tcl
@@ -0,0 +1,63 @@
+#!/bin/sh
+#\
+exec tclsh8.6 "$0" "$@"
+
+source ../ooxml.tcl
+source ../ooxml-docx.tcl
+source strings.tcl
+namespace import ::ooxml::docx::docx
+set docx [docx new]
+
+# Set a default style
+$docx style paragraphdefault -spacing {before 240 after 120}
+
+# Add two paragraphs
+$docx paragraph "A very simple monoton paragraph: $loreipsum"
+$docx paragraph "The next paragraph: $loreipsum"
+
+# What about headings and such? Well, WordprocessingML doesn't really
+# has a basic concept for that. They are (for now) just another
+# paragraph with special settings. Let's define a style for heading1:
+$docx style paragraph Heading1 -fontsize 28 -spacing {before 480 after 240}
+$docx paragraph "Looks like a Heading" -style Heading1
+
+# Add two paragraphs
+$docx paragraph "A very simple monoton paragraph: $loreipsum"
+$docx paragraph "The next paragraph: $loreipsum"
+
+# Next Heading
+$docx paragraph "The next Heading" -style Heading1
+
+# Certain parts of a paragraph may have local formating. This is done
+# by starting a paragraph and then append to that.
+$docx paragraph "The start of the first paragraph after the first Heading. "
+$docx append "Another chunk of the paragraph, just with the same formating. "
+$docx append "This chunk of the paragraph is obviously localy formated." \
+      -color 0000ff
+$docx append "You have certain effects already on your hand "
+foreach textOnOff {
+        bold
+        dstrike
+        italic 
+        strict
+} {
+    $docx append "$textOnOff " -$textOnOff on
+}
+$docx append "color " -color 00ff00
+foreach text {Serveral kinds of underline} underline {
+    single
+    words
+    double
+    thick
+    dotted
+} {
+    $docx append "$text " -underline $underline
+}
+
+$docx append " or fontsize " -fontsize 18pt
+$docx append " and you can freely combine them " -fontsize 15pt \
+    -underline double -color ff0000
+$docx append " which isn't so important because using them all would be ugly.\
+               Rest of the paragraph $loreipsum"
+$docx write docx-example1.docx
+$docx destroy

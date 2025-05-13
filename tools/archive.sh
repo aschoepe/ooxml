74255eece96211f6d5ad1bea86f8b4b8dab130f4
alex <fossil@sowaswie.de>
2025-05-13 09:33:22 +0000

    updated archive options: tar --no-xattrs --no-mac-metadata --disable-copyfile and more excludes
    FossilOrigin-Name: fe6dce99499ec1f3df9c3c46c928054f9ccd8e85aba2aefb61c0db5ad5a164d6

--- tools/archive.sh
+++ tools/archive.sh
@@ -17,9 +17,15 @@ rm -f uv/ooxml*.zip uv/ooxml*.tar.gz
 mkdir -p uv/${PACKAGE_NAME}${PACKAGE_VERSION}
 
 cp ${PKG_TCL_SOURCES} manifest.txt pkgIndex.tcl uv/${PACKAGE_NAME}${PACKAGE_VERSION}
+
+# xattr -r -d com.apple.provenance .: recursively (-r) deletes the extended attribute com.apple.provenance from the specified file or directory. com.apple.provenance: An extended attribute used by macOS (e.g., Finder or Gatekeeper) to track the origin of a file, such as downloaded sources.
+# tar --no-mac-metadata: Do not store or restore Mac extended metadata (e.g., resource forks, Finder info, and so forth). This metadata is stored in special “AppleDouble” files or as extended attributes. This option is useful when creating archives to be used on non-Mac platforms.
+# tar --disable-copyfile: This option is equivalent to setting the environment variable COPYFILE_DISABLE=1. It disables copying of extended attributes and resource forks by preventing the use of the copyfile() API. Use this when you want to avoid creation of ._* AppleDouble files or when those are not needed.
+# tar --no-xattrs: Do not store or restore extended file attributes (xattrs). These are often system or application metadata not relevant or supported on other systems.
+
 cd uv
-zip -qr ${PACKAGE_NAME}${PACKAGE_VERSION}.zip ${PACKAGE_NAME}${PACKAGE_VERSION}
-tar -czf ${PACKAGE_NAME}${PACKAGE_VERSION}.tar.gz ${PACKAGE_NAME}${PACKAGE_VERSION}
+zip -x '.DS_Store' -x '*/.DS_Store' -qr ${PACKAGE_NAME}${PACKAGE_VERSION}.zip ${PACKAGE_NAME}${PACKAGE_VERSION}
+tar --no-xattrs --no-mac-metadata --disable-copyfile --exclude='.DS_Store' --exclude='*/.DS_Store' -czf ${PACKAGE_NAME}${PACKAGE_VERSION}.tar.gz ${PACKAGE_NAME}${PACKAGE_VERSION}
 rm -fr ${PACKAGE_NAME}${PACKAGE_VERSION}
 
 cp template-download.html download.html
@@ -29,4 +35,4 @@ wq
 !
 
 cd ../..
-tar --exclude=uv/\* --exclude=doc/ECMA\* --exclude=ooxml/.fslckout --exclude=ooxml/.fossil-settings --exclude=ooxml/.gitattributes --exclude=ooxml/.vscode -czf ${PACKAGE_NAME}/uv/${PACKAGE_NAME}${PACKAGE_VERSION}-src.tar.gz ${PACKAGE_NAME}
+tar --no-xattrs --no-mac-metadata --disable-copyfile --exclude='uv/*' --exclude='doc/ECMA*' --exclude='ooxml/.fslckout' --exclude='ooxml/.fossil-settings' --exclude='ooxml/.gitattributes' --exclude='ooxml/.vscode' --exclude='.DS_Store' --exclude='*/.DS_Store' --exclude='autom4te.cache' --exclude='configure~' --exclude='config.*' -czf ${PACKAGE_NAME}/uv/${PACKAGE_NAME}${PACKAGE_VERSION}-src.tar.gz ${PACKAGE_NAME}
